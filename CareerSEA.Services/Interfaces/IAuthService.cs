87901ef82a1d9810460ced496a9a24c40f4b28f6
sarperim <ercansarperim@gmail.com>
2025-11-23 04:18:31 +0300

    Sign up Api added

---
+++ CareerSEA.Services/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using CareerSEA.Contracts.Requests;
+using CareerSEA.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerSEA.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<BaseResponse> RegisterAsync(SignupRequest request);
+    }
+}
---
+++ CareerSEA.Services/Services/AuthService.cs
@@ -0,0 +1,53 @@
+﻿using CareerSEA.Contracts.Requests;
+using CareerSEA.Contracts.Responses;
+using CareerSEA.Data;
+using CareerSEA.Data.Entities;
+using CareerSEA.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerSEA.Services.Services
+{
+    public class AuthService : IAuthService
+    {
+        public CareerSEADbContext _dbContext;
+        public AuthService(CareerSEADbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<BaseResponse> RegisterAsync(SignupRequest request)
+        {
+            if (await _dbContext.Users.AnyAsync(u => u.UserName == request.UserName))
+            {
+                return new BaseResponse
+                {
+                    Status = false,
+                    Message = "User already exists"
+                };
+            }
+            var user = new User
+            {
+                UserName = request.UserName,
+                Name = request.Name,
+                LastName = request.LastName
+            };
+            user.PasswordHash = new PasswordHasher<User>().HashPassword(user, request.Password);
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+            var response = new BaseResponse
+            {
+                Status = true,
+                Data = user
+
+            };
+
+            return response;
+        }
+    }
+}
