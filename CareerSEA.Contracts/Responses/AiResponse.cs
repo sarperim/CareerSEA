04bc01d263a72c6dbcdf1d95bc1144add325fa60
sarperim <ercansarperim@gmail.com>
2025-12-10 00:21:08 +0300

    added ai model web app rest api connection

---
+++ CareerSEA.Contracts/Responses/AiResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerSEA.Contracts.Responses
+{
+    public class AIResponse
+    {
+        public string best_job { get; set; }
+        public float match_score { get; set; }
+        public List<AIRecommendation> recommendations { get; set; }
+    }
+
+    public class AIRecommendation
+    {
+        public string label { get; set; }
+        public float score { get; set; }
+    }
+}
