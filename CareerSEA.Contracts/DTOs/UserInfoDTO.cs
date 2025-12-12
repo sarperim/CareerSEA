dee96fd4fdb81ed0746c5d9c6c727fb08501d97a
sarperim <ercansarperim@gmail.com>
2025-12-12 16:01:55 +0300

    updated full job titles for experience page

---
+++ CareerSEA.Contracts/DTOs/UserInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerSEA.Contracts.DTOs
+{
+    public class UserInfoDTO
+    {
+            public required string UserId { get; set; }
+            public required string Username { get; set; }
+    }
+}
